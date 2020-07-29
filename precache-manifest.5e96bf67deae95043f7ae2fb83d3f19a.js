9a17fbe76e5fb3f5fe8f27cb32ca5747e37a7312
Amir Martinez <airmax8@gmail.com>
2020-07-29 17:08:23 -0400

    Updates

---
+++ precache-manifest.5e96bf67deae95043f7ae2fb83d3f19a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86013cee9a6377175d624423922157a1",
+    "url": "/santodomingorp/index.html"
+  },
+  {
+    "revision": "b3b4c0c09ba3bfbd2ee8",
+    "url": "/santodomingorp/static/css/2.f2914626.chunk.css"
+  },
+  {
+    "revision": "83be886b4042f742e14a",
+    "url": "/santodomingorp/static/css/main.3e387987.chunk.css"
+  },
+  {
+    "revision": "b3b4c0c09ba3bfbd2ee8",
+    "url": "/santodomingorp/static/js/2.025d41a5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/santodomingorp/static/js/2.025d41a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83be886b4042f742e14a",
+    "url": "/santodomingorp/static/js/main.079a6d29.chunk.js"
+  },
+  {
+    "revision": "541e1712f15f065b5639",
+    "url": "/santodomingorp/static/js/runtime-main.73c9d21b.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/santodomingorp/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/santodomingorp/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/santodomingorp/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/santodomingorp/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/santodomingorp/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/santodomingorp/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/santodomingorp/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/santodomingorp/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/santodomingorp/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/santodomingorp/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/santodomingorp/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/santodomingorp/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/santodomingorp/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/santodomingorp/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/santodomingorp/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/santodomingorp/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/santodomingorp/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
